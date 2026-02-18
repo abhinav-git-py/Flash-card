3e458db52eba33e14fd8cc694c9dbf01d212fe76
abhinav-git-py <nimmapatiabhinav@gmail.com>
2026-02-18 12:30:12 +0530

    Add files via upload
    This is a simple Python flash card application built using Tkinter and Pandas.It helps students learn French words with their English meanings in an interactive way.
    
    Technologies Used
    Python
    Tkinter (GUI)
    Pandas (CSV handling)
    
     Features
    Displays French words randomly
    Automatically flips the card to show the English meaning
    Buttons to mark a word as known or unknown
    Saves unknown words to a CSV file for later practice
    Beginner-friendly project for learning GUI, CSV handling, and logic building

---
+++ main.py
@@ -0,0 +1,69 @@
+from tkinter import *
+import pandas as pd
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ------------------ DATA ------------------ #
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv("data/french_words.csv")
+
+data_dict = data.to_dict(orient="records")
+current_card = {}
+
+# ------------------ FUNCTIONS ------------------ #
+def next_card():
+    global current_card, flip_time
+
+    windows.after_cancel(flip_time)
+
+    current_card = random.choice(data_dict)
+
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front)
+
+    flip_time = windows.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=card_back)
+
+
+def next_card_wrong():
+    data_dict.remove(current_card)
+    pd.DataFrame(data_dict).to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+# ------------------ UI ------------------ #
+windows = Tk()
+windows.title("Flashy")
+windows.config(padx=40, pady=40, bg=BACKGROUND_COLOR)
+
+flip_time = windows.after(3000, flip_card)
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front = PhotoImage(file="images/card_front.png")
+card_back = PhotoImage(file="images/card_back.png")
+
+canvas_image = canvas.create_image(400, 263, image=card_front)
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+right_image = PhotoImage(file="images/right.png")
+wrong_image = PhotoImage(file="images/wrong.png")
+
+Button(image=right_image, highlightthickness=0, command=next_card).grid(row=1, column=0)
+Button(image=wrong_image, highlightthickness=0, command=next_card_wrong).grid(row=1, column=1)
+
+next_card()
+windows.mainloop()
+
+
+
+
